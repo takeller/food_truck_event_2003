282e5691445c30ea906bdcc937a4762eb9bd3b0b
Taylor Keller <takeller17@gmail.com>
2020-04-15 10:21:17 -0600

    Add check_stock test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -24,6 +24,14 @@ class FoodTruckTest < MiniTest::Test
     assert_equal 0, @food_truck.check_stock(@item1)
   end
 
+  def test_check_stock
+    @food_truck.stock(@item1, 30)
+    assert_equal 30, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+  end
+
   def test_stock
     @food_truck.stock(@item1, 30)
     expected = {@item1 => 30}
