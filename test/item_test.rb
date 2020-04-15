06f2187e86d458bec833d6ff745e86652257597a
Taylor Keller <takeller17@gmail.com>
2020-04-15 10:10:00 -0600

    Add readable attribute test

--- test/item_test.rb
+++ test/item_test.rb
@@ -13,4 +13,11 @@ class ItemTest < MiniTest::Test
     assert_instance_of Item, @item1
     assert_instance_of Item, @item2
   end
+
+  def test_it_has_readable_attributes
+    assert_equal 'Peach Pie (Slice)', @item1.name
+    assert_equal 'Apple Pie (Slice)', @item2.name
+    assert_equal "$3.75", @item1.price
+    assert_equal '$2.50', @item2.price
+  end
 end
