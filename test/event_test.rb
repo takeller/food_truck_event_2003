3f20ce5223b75ccb173a0122cf30f81a54017a00
Taylor Keller <takeller17@gmail.com>
2020-04-15 10:31:07 -0600

    No food trucks by default test

--- test/event_test.rb
+++ test/event_test.rb
@@ -14,7 +14,11 @@ class EventTest < MiniTest::Test
     assert_instance_of Event, @event
   end
 
-  def test_it_has_a_name 
+  def test_it_has_a_name
     assert_equal "South Pearl Street Farmers Market", @event.name
+  end
+
+  def test_it_has_no_food_trucks_by_default
+    assert_equal Array.new, @event.food_trucks
   end
 end
